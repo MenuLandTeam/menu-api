a67a9e52abbe6939e0c3d825f4ae66a2ea04cd24
Ali <alishakeri20003@gmail.com>
2024-12-05 21:18:36 +0330

    Resources

---
+++ app/Http/Resources/Product_Rates/ProductRateIndexResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Product_Rates;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductRateIndexResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'shop_product_id' => $this->shop_product_id,
+            'comment' => $this->comment,
+            'is_accepted' => $this->is_accepted,
+        ];
+    }
+}
---
+++ app/Http/Resources/Shop_Comments/ShopCommentIndexResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Shop_Categories;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShopCommentIndexResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'shop_product_id' => $this->shop_product_id,
+            'comment' => $this->comment,
+            'is_accepted' => $this->is_accepted,
+        ];
+    }
+}
